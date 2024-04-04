c48a781b5f6bd725ebe296172eb3b73045997afe
Smit Khobragade <100017129+Smitkhobragade@users.noreply.github.com>
2024-04-04 22:11:02 +0530

    Added Button.js

---
+++ UNIT III Assesment/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const Button = ({ onPress, title }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
+export default Button;
